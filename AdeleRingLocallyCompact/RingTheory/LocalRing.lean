a278c98f5a1290aefb31ccf8929d693e0272c907
Salvatore Mercuri <smercuri@ed.ac.uk>
2024-03-20 20:31:04 +0100

    Refactor

--- AdeleRingLocallyCompact/RingTheory/LocalRing.lean
+++
@@ -1,8 +0,0 @@
-import Mathlib
-
-variable (R : Type u) [CommRing R] [LocalRing R]
-
-def ResidueRing_order (n : ℕ) := R ⧸ (LocalRing.maximalIdeal R)^n
-
-noncomputable instance : CommRing (ResidueRing_order R n) :=
-  show CommRing (R ⧸ (LocalRing.maximalIdeal R)^n) from inferInstance
